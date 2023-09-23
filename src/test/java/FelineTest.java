2ae660e83131f3f8da6b2997d07fb5432c4cc1ba
Marina Riabogina <marina.ryabogina@yandex.ru>
2023-09-23 18:10:38 +0700

    add test for Feline

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,30 @@
+
+import com.example.Feline;
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+public class FelineTest {
+
+    @Test
+    public  void eatMeat() throws Exception{
+        List<String> food = List.of("Животные", "Птицы", "Рыба");
+        Feline feline = new Feline();
+        assertEquals(food, feline.eatMeat());
+    }
+    @Test
+    public void getFamily(){
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+    @Test
+    public void getKittensWithoutParameterReturnOne(){
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens());
+    }
+    @Test
+    public void getKittensWithParameterReturnParameter(){
+        Feline feline = new Feline();
+        int numberOfKittens = 5;
+        assertEquals(numberOfKittens, feline.getKittens(numberOfKittens));
+    }
+}
